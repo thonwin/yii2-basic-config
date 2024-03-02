57bbb3c13b0452932330a2cea49f91db7285fae5
thonwin <rpongtho@mail.wu.ac.th>
2024-03-02 15:05:40 +0700

    unit test

---
+++ tests/unit/models/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tests\unit\models;
+class UserTest extends \Codeception\Test\Unit
+{
+    public function testValidation()
+        {
+            $this ->assertFalse(false);
+            $this ->assertTrue(true);
+        }
+} 
